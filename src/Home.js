c84e87036bd8ade06074bcfb5304e4d0ba256976
Hemanth-simha <connect2hemanthsimha@gmail.com>
2022-06-04 18:24:30 +0530

    another commit

--- src/Home.js
+++ src/Home.js
@@ -6,12 +6,15 @@ const Home = () => {
     let history = useHistory();
 
     let [stdName , setName] = useState("");
+    let [email , setEmail] = useState("");
+    let [phone , setPhone] = useState("");
+    let [age , setAge] = useState("");
     let [gender , setGender] = useState("");
 
     let handelSubmit = (e)=>{
         e.preventDefault();
 
-        let newStd = {stdName , gender};
+        let newStd = {stdName,email,phone,age , gender};
 
         fetch("http://localhost:4000/StudentDetails" , 
         {
@@ -22,39 +25,39 @@ const Home = () => {
         .then(()=>{
             alert("data is added");
             history.push("/sheets");
-
         })
-    
     }
 
 
 
     return ( 
         <div className="home-content">
+            <form onSubmit={handelSubmit} className="form-content">
+                    <label>Name : </label> <input type="text" value={stdName} onChange={(e)=>{setName(e.target.value)}} />
 
-<form onSubmit={handelSubmit}>
+                    <label>Email : </label> <input type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} />
 
-                <label>Name : </label> 
-    <input type="text" value={stdName} onChange={(e)=>{setName(e.target.value)}} />
+                    <label>Phone : </label> <input type="tel" value={phone} onChange={(e)=>{setPhone(e.target.value)}} />
+
+                    <label>Age : </label> <input type="number" value={age} onChange={(e)=>{setAge(e.target.value)}} />
+
+                    <label>Gender : </label>
+
+                    <fieldset>
+                        <legend align="center">Select your gender</legend>
+
+                        <div><input type="radio" name="gender" value="Male"  id='male' onClick={(e)=>{setGender(e.target.value)}} /> <label htmlFor='male'>Male</label></div>
+
+                        <div><input type="radio" name="gender" value="Female" id='female' onClick={(e)=>{setGender(e.target.value)}}/> <label htmlFor='female'>Female</label></div>
 
 
-                <fieldset>
-                    <legend align="center">Select your gender</legend>
+                        <div><input type="radio" name="gender" value="Others" id='others' onClick={(e)=>{setGender(e.target.value)}}/> <label htmlFor='others'>Others</label></div>
 
-<input type="radio" name="gender" value="Male" onClick={(e)=>{setGender(e.target.value)}} /> <label>Male</label>
-
-<input type="radio" name="gender" value="Female" onClick={(e)=>{setGender(e.target.value)}}/> <label>Female</label>
-
-
-<input type="radio" name="gender" value="Others" onClick={(e)=>{setGender(e.target.value)}}/> <label>Others</label>
-
-                    <br />
-                </fieldset>
-
-                <input type="submit" />
-                
-</form>
-
+                        <br />
+                    </fieldset>
+                    
+                    <input type="submit" />    
+            </form>
         </div>
     );
 }
--- src/Sheets.js
+++ src/Sheets.js
@@ -4,28 +4,42 @@ import {Link} from 'react-router-dom'
 const Sheets = () => {
 
     let [data , setData] = useState(null);
+    let [pending , setPending] = useState(true);
 
+    setTimeout(()=>{
     fetch("http://localhost:4000/StudentDetails")
     .then((response)=>{return response.json();})
-    .then((fetchedData)=>{setData(fetchedData);})
+    .then((fetchedData)=>{setData(fetchedData);setPending(false)})
+    } , 2000)
 
     return ( 
-    <div>
+    <div className="sheet-content">
+
+        {
+            pending && <h1>Loading</h1>
+        }
+
         {
             data && 
-            <table border="2px" className="table-content">
-                <tr>
+            <table border="2px">
+                <tr className="first-row">
                     <th>sl no</th>
                     <th>Student Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Age</th>
                     <th>Gender</th>
                     <th>View </th>
                 </tr>
 
                 {
-                    data.map((std)=>(
+                    data.map((std , i)=>( 
                         <tr>
-                            <td>{std.id}</td>
+                            <td>{i+1}</td>
                             <td>{std.stdName}</td>
+                            <td>{std.email}</td>
+                            <td>{std.phone}</td>
+                            <td>{std.age}</td>
                             <td>{std.gender}</td>
                             <td><Link to={`/details${std.id}`}><button>View</button></Link></td>
                         </tr>
@@ -34,6 +48,7 @@ const Sheets = () => {
 
             </table>
         }
+
     </div>
     );
 }
